38e62c72d6d54fdf56ad54237b2ccb715f0502c1
Kevin Wong <kevin@grata.com>
2023-11-15 00:03:29 -0500

    Update test example 2

--- rehearser_examples/examples/example1/tests/test_main_methods.py
+++ rehearser_examples/examples/example1/tests/test_main_methods.py
@@ -8,6 +8,7 @@ def main_method(i):
     print(f"Main! -> {i}")
     return i
 
+
 # Rehersal
 rm = RehearserMethod(main_method)
 with patch("__main__.main_method", rm.get_proxy_method()):
@@ -15,13 +16,17 @@ with patch("__main__.main_method", rm.get_proxy_method()):
     main_method(2)
     main_method(3)
 
-rm.set_interactions_file_directory("./rehearser_examples/examples/example2/tests/raw_files/")
+rm.set_interactions_file_directory(
+    "./rehearser_examples/examples/example2/tests/raw_files/"
+)
 rm.write_interactions_to_file()
 
 # Unit test
-mg = MockGenerator(rm.get_interactions())
+mg = MockGenerator(
+    interactions_src="./rehearser_examples/examples/example1/tests/raw_files/main_method/latest_interactions.json"
+)
 m1 = mg.create_mock()
 with patch("__main__.main_method", m1):
-    assert(main_method(), 1)
-    assert(main_method(), 2)
-    assert(main_method(), 3)
+    assert (main_method(), 1)
+    assert (main_method(), 2)
+    assert (main_method(), 3)
