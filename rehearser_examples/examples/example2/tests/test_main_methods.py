2bd863e10115baa1dbbb342ee466bc47f8c81bc2
Kevin Wong <kevin@grata.com>
2023-11-14 23:41:09 -0500

    Update test example 2

--- rehearser_examples/examples/example2/tests/test_main_methods.py
+++ rehearser_examples/examples/example2/tests/test_main_methods.py
@@ -5,33 +5,23 @@ from rehearser.rehearser_method import RehearserMethod
 
 
 def main_method(i):
-    print(f"{i}: I am Main!")
+    print(f"Main! -> {i}")
     return i
 
-
+# Rehersal
 rm = RehearserMethod(main_method)
-
-# rehersal
 with patch("__main__.main_method", rm.get_proxy_method()):
     main_method(1)
     main_method(2)
     main_method(3)
 
-print(f"rm._method: {rm._method}")
-print(f"rm.get_interactions(): {rm.get_interactions()}")
-
-rm.set_interactions_file_directory("./tests/raw_files/")
+rm.set_interactions_file_directory("./rehearser_examples/examples/example2/tests/raw_files/")
 rm.write_interactions_to_file()
 
-# make sure 2 mocks from one mg have it own interactions queue
+# Unit test
 mg = MockGenerator(rm.get_interactions())
 m1 = mg.create_mock()
-m2 = mg.create_mock()
 with patch("__main__.main_method", m1):
-    print(main_method())
-    print(main_method())
-    print(main_method())
-with patch("__main__.main_method", m2):
-    print(main_method())
-    print(main_method())
-    print(main_method())
+    assert(main_method(), 1)
+    assert(main_method(), 2)
+    assert(main_method(), 3)
